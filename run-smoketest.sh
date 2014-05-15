ee6c70f71cdc44451bed6880551f260d9c4560e0
Marek Novotny <hotmana76@gmail.com>
2014-05-15 12:04:21 +0200

    fixing running smoketest launcher for individual package and test deployment

--- run-smoketest.sh
+++ run-smoketest.sh
@@ -4,10 +4,10 @@
 
 export SMOKE_TEST_OPTS="-DwfkMavenRepoUrl=file://$WORKSPACE/jboss-wfk-maven-repository -DeapMavenRepoUrl=file://$WORKSPACE/jboss-eap-maven-repository -Dmaven.repo.local=$WORKSPACE/.repository -s $WORKSPACE/settings.xml"
 
-mvn -f $1 clean install $SMOKE_TEST_OPTS
+mvn -U -f $1 clean install $SMOKE_TEST_OPTS
 
-#mvn org.jboss.as.plugins:jboss-as-maven-plugin:7.4.Final:deploy $SMOTE_TEST_OPTS
+mvn -f $1 org.jboss.as.plugins:jboss-as-maven-plugin:7.4.Final:deploy $SMOTE_TEST_OPTS
 
-#sleep 1m
+sleep 1m
 
-#mvn org.jboss.as.plugins:jboss-as-maven-plugin:7.4.Final:undeploy $SMOKE_TEST_OPTS
+mvn -f $1 org.jboss.as.plugins:jboss-as-maven-plugin:7.4.Final:undeploy $SMOKE_TEST_OPTS
